9b652d62b930c67c3cd55ab0ac536893526d99b5
sirkenedy <owoputokehinde@gmail.com>
2021-04-03 14:16:46 +0100

    testing slack 2

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    // return $request->user();
+    return $request->user();
 });
